43ecaf55ab7c0a142a1a15427c818bae149af526
Natascha Schulz <natascha-schulz@hotmail.com>
2015-07-06 16:36:41 +0200

    erste Version des Games

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public float velocityMultiplier = 4.0f;
+
+	private GameObject launchPoint;
+	public GameObject prefabProjectile;
+
+	private bool aimingMode;
+	private Vector3 launchPos;
+
+	private GameObject projectile;
+
+
+
+
+	void Awake(){
+
+		Transform launchPointTrans = transform.Find("launchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+
+		launchPos = launchPointTrans.position;
+	}
+
+	void OnMouseEnter(){
+		launchPoint.SetActive (true);
+		//print ("Yaay, the mouse has entered!");
+	}
+
+	void OnMouseExit(){
+		launchPoint.SetActive (false);
+		//print ("Ooh no, the mouse has exited!");
+	}
+
+	void OnMouseDown(){
+		aimingMode = true;
+		projectile = Instantiate (prefabProjectile)as GameObject;
+		projectile.transform.position = launchPos;
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
+	}
+    void Update(){
+
+
+		// if we are not in aiming mode then do nothing
+		if(aimingMode == false){
+			return;
+		}
+
+		// get the current mouse position in screen coordinates
+		Vector3 mousePos2D = Input.mousePosition;
+
+		// convert mouse position to 3d world space
+		mousePos2D.z = - Camera.main.transform.position.z;
+		Vector3 mousePos3D = - Camera.main.ScreenToWorldPoint (mousePos2D);
+	 
+		// find the difference between the launch position and my mouse position
+		Vector3 mouseDelta = launchPos - mousePos3D;
+
+		// move the projectile to this new postion
+		float radius = GetComponent<SphereCollider>().radius;
+		mouseDelta = Vector3.ClampMagnitude (mouseDelta, radius);
+
+		projectile.transform.position = launchPos + mouseDelta;
+
+		// if the mousebutton has been released, 
+		if(Input.GetMouseButtonUp(0)){
+
+		// exit aimingmode
+			aimingMode = false;
+
+		// stop the projectile from being kinematic
+		projectile.GetComponent<Rigidbody>().isKinematic = false;
+
+		// set the projectiles velocity to the negative mouseDelta
+		projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMultiplier;
+
+		followCam.S.poi = projectile;
+		}
+	}
+}
+
---
+++ Assets/followCam.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followCam : MonoBehaviour {
+
+	static public followCam S; //single
+
+	public GameObject poi; // point of interest
+
+	private float camZ;
+
+	public float easing = 0.05f;
+
+
+	// awake is what's being called when the start button is pressed
+
+	void Awake(){
+		S = this;
+		camZ = transform.position.z;
+
+	}
+
+	void Update(){
+		// if there is no poi just get outta here!
+		if (poi == null){
+			return;
+		}
+
+		Vector3 destination = poi.transform.position;
+
+		destination.x = Mathf.Max (0.0f, destination.x);
+		destination.y = Mathf.Max (0.0f, destination.y);
+	
+
+		destination = Vector3.Lerp (transform.position, destination, easing);
+
+		destination.z = camZ;
+
+		transform.position = destination;
+
+		GetComponent<Camera> ().orthographicSize = 10 + destination.y;
+	}
+}
